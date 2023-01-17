6f9516427d97678c4b316d57d443665525a51a72
Alice <darondeau.alice@gmail.com>
2023-01-17 16:57:15 +0100

    remplacer les images par une image de l'internet Lama + quote dans le pop-up qui se déclenche au click du bouton

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-async function fetchData(){
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'ee7279b9bbmsh7172f99cef0519fp17ca96jsn6ac28430c532',
-            'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-        }
-    };
+//fonction qui remplace les images de la page par des lamas
+function transformImages() {
+
+    let fileNames = [
+        "images/lama_1.jpg",
+        "images/lama_2.jpg",
+        "images/lama_3.jpg",
+        "images/lama_4.jpg",
+        "images/lama_5.jpg",
+        "images/lama_6.jpg",
+        "images/lama_7.jpg",
+        "images/lama_8.jpg",
+    ];
+      
+    //on récup tous les éléments qui ont le tag "img" dans la tab ouverte
+    let imgs = document.getElementsByTagName("img");
+      
+    //on parcourt toutes les images trouvées pour changer leurs URLS
+    for (var i =0 ; i < imgs.length ; i++) {
+        console.log(imgs[i]);
     
-    const res = await fetch('https://quotes15.p.rapidapi.com/quotes/random/?language_code=en', options)
-    const record = await res.json();
-
-    console.log('record', record);
-
-    document.getElementById("quotes").innerHTML = record.content;
+        //on génère une image parmi notre bdd
+        let random = Math.floor(Math.random() * fileNames.length);
+        let file = fileNames[random];
+        let url = file.url ;
+        //console.log(file);
+        imgs[i].src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.santelog.com%2Fsites%2Fsantelog.com%2Fwww.santelog.com%2Ffiles%2Fimages%2Faccroche%2Fadobestock_276208008_lama.jpeg&f=1&nofb=1&ipt=0c74cb072050685502d5c19b369a6e8586de258daa09d477b81d0d1ca8eadd92&ipo=images";
+        //"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.santelog.com%2Fsites%2Fsantelog.com%2Fwww.santelog.com%2Ffiles%2Fimages%2Faccroche%2Fadobestock_276208008_lama.jpeg&f=1&nofb=1&ipt=0c74cb072050685502d5c19b369a6e8586de258daa09d477b81d0d1ca8eadd92&ipo=images"
+    }
 }
 
-fetchData()
+transformImages();
+
+
